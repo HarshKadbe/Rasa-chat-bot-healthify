6a069a15c10aa3d30987b50af8265bb8ec2d08ed
Harsh Kadbe <102993879+HarshKadbe@users.noreply.github.com>
2025-03-06 18:08:51 +0530

    Update settings.py

--- healthify/settings.py
+++ healthify/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6b_1utqrth&33c@rv7y#5n*fw@iq^i)*bdln10+v5%yxcd)26t'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
